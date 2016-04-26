42db1d85a3f9895eae1a4bc4d375287a8d24db77
Anton <a.kiyatkin@itlf.ru>
2016-04-26 17:22:12 +0400

    fix namespaces

--- subscribe.php
+++ subscribe.php
@@ -1,35 +1,42 @@
 <?php
+use infrajs\config\Config;
+use infrajs\ans\Ans;
+use infrajs\path\Path;
+use infrajs\load\Load;
+use infrajs\access\Access;
+use infrajs\template\Template;
+use infrajs\mail\Mail;
 
 $ans = array();
 $ans['popup'] = true;
+$conf = Config::get('subscribe');
 if (empty($_REQUEST['email'])) {
-	$conf=infra_config();
-	return infra_err($ans, $conf['subscribe']['msg']);
+	return Ans::err($ans, $conf['msg']);
 }
 if (strlen($_REQUEST['email']) > 1000) {
-	return infra_err($ans, 'Слишком много данных. '.$conf['subscribe']['msg']);
+	return Ans::err($ans, 'Слишком много данных. '.$conf['msg']);
 }
 $email = strip_tags($_REQUEST['email']);
 $email = trim($email);
-$email = infra_forFS($email);
+$email = Path::encode($email);
 $agent = $_SERVER['HTTP_USER_AGENT'];
 $ip = $_SERVER['REMOTE_ADDR'];
 
 session_start();
 if (empty($_SESSION['submit_time'])) $_SESSION['submit_time'] = 0;			
-if (time() - $_SESSION['submit_time'] < 60) return infra_err($ans, 'Письмо уже отправлено! Новое сообщение можно будет отправить через 1 минуту!');
+if (time() - $_SESSION['submit_time'] < 60) return Ans::err($ans, 'Письмо уже отправлено! Новое сообщение можно будет отправить через 1 минуту!');
 $_SESSION['submit_time'] = time();
 
 
-$data=array('email' => $email, 'agent' => $agent, 'ip' => $ip, 'host' => $_SERVER['HTTP_HOST']);
-$body = infra_template_parse('*order.tpl', $data);
-infra_mail_toAdmin('Запрос '.$data['host'].' '.$email, 'noreplay@'.$data['host'], $body);
+$data = array('email' => $email, 'agent' => $agent, 'ip' => $ip, 'host' => $_SERVER['HTTP_HOST']);
+$body = Template::parse('-subscribe/subscribe.mail.tpl', $data);
+Mail::toAdmin('Запрос '.$data['host'].' '.$email, 'noreplay@'.$data['host'], $body);
 
-$dirs=infra_dirs();
-$src=$dirs['data'].'.subscribe.json';
-$subs=infra_loadJSON($src);
-if(!$subs)$subs=array();
+
+$src = Path::resolve('~.subscribe.json');
+$subs = Load::loadJSON($src);
+if (!$subs) $subs=array();
 $subs[$email]=date('d.m.Y');
-file_put_contents($src, infra_json_encode($subs));
+file_put_contents($src, Load::json_encode($subs));
 
-return infra_ret($ans, 'Ваша заявка принята');
+return Ans::ret($ans, 'Ваша заявка принята');
